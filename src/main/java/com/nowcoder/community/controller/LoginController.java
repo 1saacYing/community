33e6491eac0359d443a7fd459fedf1b70c35cb3a
IsaacYing <yxj1saac@gmail.com>
2025-06-28 17:57:59 +0800

    代码开发-2.2 开发注册功能-2 添加动态配置，显示错误信息

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -27,6 +27,7 @@ public class LoginController {
 
     // 如果页面传入的值与user的属性匹配，springMVC会自动将值注入到对应的属性
     // Model model：Spring MVC 提供的对象，用于向视图层传递数据（相当于把数据传到 HTML 页面
+    // [2.2 开发注册功能-2]Spring MVC 的自动数据绑定机，会隐式调用model.addAttribute("user", user)，Thymeleaf 页面仍可通过 ${user} 访问
     public String register(Model model, User user) {
         Map<String, Object> map = userService.register(user);
         if (map == null || map.isEmpty()) {
@@ -36,6 +37,8 @@ public class LoginController {
             // 返回一个结果页面
             return "/site/operate-result";
         } else {
+            // [2.2 开发注册功能-2] 建议显式添model.addAttribute("user", user)
+            model.addAttribute("user", user);
             model.addAttribute("usernameMsg", map.get("usernameMsg"));
             model.addAttribute("passwordMsg", map.get("passwordMsg"));
             model.addAttribute("emailMsg", map.get("emailMsg"));
